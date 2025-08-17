1143b9380a6c599c155521b7ee593fb9e8ac1c74
이태란 <dalbogi5@gmail.com>
2025-08-17 18:41:45 +0900

    feat:navigation width 변경

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Login from "./pages/Login.jsx";
 import Register from "./pages/Register.jsx";
 import Mypage from "./pages/Mypage.jsx";
 import Navigation from "./components/Navigation.jsx";
+import Layout from "./components/Layout.jsx";
 
 export default function App() {
   const location = useLocation();
@@ -26,24 +27,29 @@ export default function App() {
   const hideNav = ["/login", "/register"].includes(location.pathname); // 로그인/회원가입에서 숨김
 
   return (
-    <div className="min-h-screen bg-slate-50 pb-24">
-      <Routes>
-        <Route path="/" element={<Intro />} />
-        <Route path="/record" element={<div className="p-6">녹음 페이지</div>} />
-        <Route path="/mypage" element={<Mypage />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="*" element={<div className="p-6">Not Found</div>} />
-      </Routes>
-
-      {!hideNav && (
-        <div className="fixed inset-x-0 bottom-4 px-4">
-          <Navigation
-            active={active}
-            onChange={(key) => navigate(keyToPath[key])}
+    <Layout>
+      <div className="min-h-screen bg-slate-50 pb-[calc(88px+env(safe-area-inset-bottom))]">
+        <Routes>
+          <Route path="/" element={<Intro />} />
+          <Route
+            path="/record"
+            element={<div className="p-6">녹음 페이지</div>}
           />
-        </div>
-      )}
-    </div>
+          <Route path="/mypage" element={<Mypage />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="*" element={<div className="p-6">Not Found</div>} />
+        </Routes>
+
+        {!hideNav && (
+          <div className="absolute inset-x-4 bottom-4">
+            <Navigation
+              active={active}
+              onChange={(key) => navigate(keyToPath[key])}
+            />
+          </div>
+        )}
+      </div>
+    </Layout>
   );
 }
