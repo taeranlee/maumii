b86985f5828448adbde85b37392513daf96a09fa
yjungs2 <100326172+yjungs2@users.noreply.github.com>
2025-08-18 21:35:45 +0900

    feat: mypage 에서 이어지는 페이지 작업

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,10 +1,19 @@
-export default function Title({ children, variant = "default", className = "" }) {
-  const base = "font-bold text-center mb-6 " + className;
+export default function Title({ children, variant = "default", className = "", icon }) {
+  const base = "font-bold text-center relative " + className;
 
   const styles = {
     auth: "m-20 text-4xl text-primary " + base,   // 로그인/회원가입용
     default: "text-2xl text-black " + base,      // 일반 페이지용
   };
 
-  return <div className={styles[variant]}>{children}</div>;
+  return (
+    <div className={`${styles[variant]} ${className}`}>
+      {icon && (
+        <span className="absolute left-4 top-1/2 transform -translate-y-1/2">
+          {icon}
+        </span>
+      )}
+      <span>{children}</span>
+    </div>
+  );
 }
