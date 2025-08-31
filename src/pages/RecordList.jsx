8a0c2b7d658b8cf03c914751855d988c88d69b7f
이태란 <dalbogi5@gmail.com>
2025-08-31 17:53:17 +0900

    feat: recordList userId 받아오는 코드 추가

--- src/pages/RecordList.jsx
+++ src/pages/RecordList.jsx
@@ -6,6 +6,7 @@ import { FaRegTrashAlt } from "react-icons/fa";
 import CalendarBottomSheet from "../components/CalendarBottomSheet";
 import Header from "../components/header";
 import ConfirmModal from "../components/ConfirmModal";
+import { useAuth } from "../context/AuthContext";
 
 // utils/dateFormat.js
 export function formatKoreanDateTime(dateString) {
@@ -23,7 +24,11 @@ export function formatKoreanDateTime(dateString) {
   });
 }
 
-export default function RecordList({uId="codus1214"}) { // react state 에서 uId 값 가져오기
+export default function RecordList() {
+  // react state 에서 uId 값 가져오기
+  const { user } = useAuth();
+  const uId = user?.uId;
+
   const navigate = useNavigate();
   // 녹음 리스트 (API 연동)
   const [recordList, setRecordList] = useState([]); // 화면에 보여줄 리스트
@@ -34,7 +39,7 @@ export default function RecordList({uId="codus1214"}) { // react state 에서 uI
   const [selectedDate, setSelectedDate] = useState(null); // Date 객체 (자정 기준)
   // 리스트 검색
   const [open, setOpen] = useState(false); // 검색창 열고 닫기
-  const [keyword, setKeyword] = useState("");  // input 값
+  const [keyword, setKeyword] = useState(""); // input 값
   // 리스트 수정
   const [editingId, setEditingId] = useState(null);
   // 리스트 삭제
@@ -42,14 +47,16 @@ export default function RecordList({uId="codus1214"}) { // react state 에서 uI
   const [targetRecordListId, setTargetRecordListId] = useState(null);
 
   useEffect(() => {
+    if (!uId) return; // 로그인 정보 없으면 실행 X
     // 백엔드 API 호출
-    axios.get(`http://localhost:9000/api/records/${uId}/record-list`)
-      .then(res => {
+    axios
+      .get(`http://localhost:9000/api/records/${uId}/record-list`)
+      .then((res) => {
         console.log("API 응답 테스트 : ", res.data);
         setRecordList(res.data); // 화면용
         setAllRecords(res.data); // 저장용
       })
-      .catch(err => console.error(err));
+      .catch((err) => console.error(err));
   }, [uId]);
 
   const handleSearch = () => {
@@ -58,22 +65,27 @@ export default function RecordList({uId="codus1214"}) { // react state 에서 uI
       return;
     }
     // 백엔드 컨트롤러 호출 ... 단어 검색
-    axios.get(`http://localhost:9000/api/records/${uId}/record-list/${encodeURIComponent(keyword)}`)
-      .then(res => {
-        if(res.data.length === 0){
+    axios
+      .get(
+        `http://localhost:9000/api/records/${uId}/record-list/${encodeURIComponent(
+          keyword
+        )}`
+      )
+      .then((res) => {
+        if (res.data.length === 0) {
           setRecordList(allRecords);
         } else {
           setRecordList(res.data);
         }
       })
-      .catch(err => console.error(err));
-    }
+      .catch((err) => console.error(err));
+  };
 
-    const handleIconClick = () => {
-      if(!open) {
-        setOpen(true);
-      } else{
-        handleSearch();
+  const handleIconClick = () => {
+    if (!open) {
+      setOpen(true);
+    } else {
+      handleSearch();
     }
   };
 
@@ -100,9 +112,7 @@ export default function RecordList({uId="codus1214"}) { // react state 에서 uI
       // allRecords도 같이 업데이트
       setAllRecords((prev) =>
         prev.map((r) =>
-          r.rlId === rlId
-            ? { ...updated, rlText: r.rlText }
-            : r
+          r.rlId === rlId ? { ...updated, rlText: r.rlText } : r
         )
       );
 
@@ -110,26 +120,29 @@ export default function RecordList({uId="codus1214"}) { // react state 에서 uI
     } catch (err) {
       console.error("수정 실패:", err);
     }
-    };
+  };
 
-    const handleDelete = async () => {
-      if(!targetRecordListId) return;
-      try {
-        await axios.delete(
-          `http://localhost:9000/api/records/record-list/${targetRecordListId}`
-        );
-        // recordList에서 삭제
-        setRecordList((prev) => prev.filter((r) => r.rlId !== targetRecordListId));
-        // allRecords에서도 삭제
-        setAllRecords((prev) => prev.filter((r) => r.rlId !== targetRecordListId));
-        setOpenDeleteModal(false); // 삭제 모달 닫기
-        setTargetRecordListId(null);
-      } catch (err) {
-        console.error("삭제 실패:", err);
-        alert("녹음 목록 삭제 중 오류가 발생했습니다.");
-      }
-    };
-  
+  const handleDelete = async () => {
+    if (!targetRecordListId) return;
+    try {
+      await axios.delete(
+        `http://localhost:9000/api/records/record-list/${targetRecordListId}`
+      );
+      // recordList에서 삭제
+      setRecordList((prev) =>
+        prev.filter((r) => r.rlId !== targetRecordListId)
+      );
+      // allRecords에서도 삭제
+      setAllRecords((prev) =>
+        prev.filter((r) => r.rlId !== targetRecordListId)
+      );
+      setOpenDeleteModal(false); // 삭제 모달 닫기
+      setTargetRecordListId(null);
+    } catch (err) {
+      console.error("삭제 실패:", err);
+      alert("녹음 목록 삭제 중 오류가 발생했습니다.");
+    }
+  };
 
   /* ---------- utils ---------- */
   function toYMD(d) {
@@ -143,7 +156,12 @@ export default function RecordList({uId="codus1214"}) { // react state 에서 uI
     <div className="flex flex-col h-screen bg-background">
       <div className="mx-auto w-full h-40">
         {/* 헤더 */}
-        <Header title="녹음 기록" center={true} shadow={true} fix={true}></Header>
+        <Header
+          title="녹음 기록"
+          center={true}
+          shadow={true}
+          fix={true}
+        ></Header>
         {/* 탭 + 검색 */}
         <div className="pl-8 pr-6 pt-5">
           <div className="mb-4 flex items-center justify-between">
@@ -175,23 +193,27 @@ export default function RecordList({uId="codus1214"}) { // react state 에서 uI
                 날짜
               </button>
             </div>
-            
+
             {/* 검색창 */}
             <div className="relative flex-2 items-center">
               <div
                 className={`flex items-center h-10 border border-purple-300 bg-white shadow-[0_0_8px_rgba(166,144,255,0.7)]
                 transition-all duration-500 ease-in-out overflow-hidden
-                ${open ? "w-44 pl-3 pr-2 rounded-full" : "w-10 justify-center rounded-full"}`}
+                ${
+                  open
+                    ? "w-44 pl-3 pr-2 rounded-full"
+                    : "w-10 justify-center rounded-full"
+                }`}
               >
                 <input
                   type="text"
                   placeholder="검색어 입력"
                   className={`text-sm outline-none transition-opacity duration-300
                   ${open ? "opacity-100 w-full" : "opacity-0 w-0"}`}
-                  onChange={e => setKeyword(e.target.value)}
+                  onChange={(e) => setKeyword(e.target.value)}
                   onFocus={() => setOpen(true)}
                   onBlur={() => setOpen(false)}
-                  onKeyDown={e => {
+                  onKeyDown={(e) => {
                     if (e.key === "Enter") handleSearch(); // 엔터로도 검색 가능
                   }}
                 />
@@ -204,7 +226,7 @@ export default function RecordList({uId="codus1214"}) { // react state 에서 uI
           </div>
         </div>
       </div>
-      
+
       <div className="flex-1 overflow-y-auto px-2 pt-6 pb-28">
         {/* 리스트 */}
         <ul className="space-y-4">
@@ -236,7 +258,9 @@ export default function RecordList({uId="codus1214"}) { // react state 에서 uI
                               const newName = e.target.value;
                               setRecordList((prev) =>
                                 prev.map((r) =>
-                                  r.rlId === rec.rlId ? { ...r, rlName: newName } : r
+                                  r.rlId === rec.rlId
+                                    ? { ...r, rlName: newName }
+                                    : r
                                 )
                               );
                             }}
@@ -245,7 +269,7 @@ export default function RecordList({uId="codus1214"}) { // react state 에서 uI
                             className="w-24 text-base font-semibold text-slate-900 border-b border-slate-300 outline-none"
                             onClick={(e) => e.stopPropagation()} // 여기서 부모 클릭 전파 막기
                           />
-                          <button 
+                          <button
                             onClick={(e) => {
                               e.stopPropagation();
                               handleUpdate(rec.rlId, rec.rlName);
@@ -259,7 +283,7 @@ export default function RecordList({uId="codus1214"}) { // react state 에서 uI
                         </>
                       ) : (
                         <>
-                          <h3 className="text-base font-semibold text-slate-900 cursor-pointer" >
+                          <h3 className="text-base font-semibold text-slate-900 cursor-pointer">
                             {rec.rlName}
                           </h3>
                         </>
@@ -286,7 +310,11 @@ export default function RecordList({uId="codus1214"}) { // react state 에서 uI
                         className="grid h-10 w-10 place-items-center rounded-full bg-button-edit shadow-inner"
                         title="이름 변경"
                       >
-                        <svg viewBox="0 0 24 24" className="h-5 w-5" fill="white">
+                        <svg
+                          viewBox="0 0 24 24"
+                          className="h-5 w-5"
+                          fill="white"
+                        >
                           <path d="M3 17.25V21h3.75l11-11.03-3.75-3.75L3 17.25Zm17.71-10.96a1 1 0 0 0 0-1.41l-2.59-2.59a1 1 0 0 0-1.41 0l-1.83 1.83 3.75 3.75 2.08-1.58Z" />
                         </svg>
                       </button>
@@ -300,7 +328,10 @@ export default function RecordList({uId="codus1214"}) { // react state 에서 uI
                       }`}
                     >
                       <button
-                        onClick={() => {setTargetRecordListId(rec.rlId); setOpenDeleteModal(true);}}
+                        onClick={() => {
+                          setTargetRecordListId(rec.rlId);
+                          setOpenDeleteModal(true);
+                        }}
                         className="grid h-10 w-10 place-items-center rounded-full bg-rose-500 shadow-inner"
                         title="삭제"
                       >
@@ -314,7 +345,7 @@ export default function RecordList({uId="codus1214"}) { // react state 에서 uI
           })}
         </ul>
       </div>
-      
+
       {/* 날짜 탭일 때 달력 Bottom Sheet */}
       <CalendarBottomSheet
         open={tab === "date" && showCal}
@@ -322,8 +353,8 @@ export default function RecordList({uId="codus1214"}) { // react state 에서 uI
         selected={selectedDate}
         setSelected={(d) => {
           setSelectedDate(d);
-          const filtered = allRecords.filter((rec) => 
-            toYMD(new Date(rec.updateDate)) === toYMD(d)
+          const filtered = allRecords.filter(
+            (rec) => toYMD(new Date(rec.updateDate)) === toYMD(d)
           );
           setRecordList(filtered);
           setShowCal(false);
@@ -331,16 +362,15 @@ export default function RecordList({uId="codus1214"}) { // react state 에서 uI
       />
 
       <ConfirmModal
-          isOpen={openDeleteModal}
-          title="정말 삭제하시겠습니까?"
-          description="상대와의 전체 녹음 기록이 사라집니다."
-          onConfirm={handleDelete}
-          onCancel={() => {
-              setOpenDeleteModal(false);
-              setTargetRecordListId(null);
-          }}
+        isOpen={openDeleteModal}
+        title="정말 삭제하시겠습니까?"
+        description="상대와의 전체 녹음 기록이 사라집니다."
+        onConfirm={handleDelete}
+        onCancel={() => {
+          setOpenDeleteModal(false);
+          setTargetRecordListId(null);
+        }}
       />
-
     </div>
   );
 }
